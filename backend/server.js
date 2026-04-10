5f3b628849f52a201ed1f65c024ca297506c9c40
icbpartners2025-art <icbpartners2025@gmail.com>
2026-04-10 17:27:49 +0200

    commit

--- backend/server.js
+++ backend/server.js
@@ -23,14 +23,11 @@ app.get("/", (req, res) => {
 });
 
 // MongoDB Connection
+const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
-
-// Start Server
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+  })
+  .catch((err) => console.error("Could not connect to MongoDB", err));
