ad6d3f03b3b9c4fb216c1d93df17bb0f34b5d968
TinasheMakanda <symphonytone@gmail.com>
2026-04-23 02:13:20 +0200

    Update Service model to include price and is_active fields

--- my_django_packages/django-base/base/models.py
+++ my_django_packages/django-base/base/models.py
@@ -47,6 +47,8 @@ class Service(models.Model):
     name = models.CharField(max_length=255)
     category = models.CharField(max_length=100) # Personal, Business, CIPC
     description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
